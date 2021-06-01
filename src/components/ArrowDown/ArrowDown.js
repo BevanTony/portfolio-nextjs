e0089b64a13727c0f946b28dcb7422cf106a8e26
Bevan Tony Medrano <babantoto@gmail.com>
2021-06-01 10:26:15 +0800

    animation framer motion

--- src/components/ArrowDown/ArrowDown.js
+++ src/components/ArrowDown/ArrowDown.js
@@ -1,14 +1,11 @@
 import  styles from './ArrowDown.module.scss'
-import Link from 'next/link'
 import { AiOutlineArrowDown } from 'react-icons/ai'
 
 
 const ArrowDown = () => {
     return(
         <div className={styles.arrowDownComponent}>
-            <Link href={''}>
                 <AiOutlineArrowDown/>
-            </Link>
         </div>
     )
 }
