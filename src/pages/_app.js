5f41cca9c256e73064ed945b22d797a6d677bf9e
Bevan Tony Medrano <babantoto@gmail.com>
2021-06-05 11:22:47 +0800

    try

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,5 @@
 import '../styles/globals.scss'
-import '../components/Landing/Landing.module.scss'
-import './About/About.module.scss'
+import '../styles/Home.module.scss'
 import Navbar from '../components/Navbar/Navbar'
 import { AnimatePresence } from 'framer-motion'
 
